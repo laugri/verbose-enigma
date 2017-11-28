eff1af2698470ab05e875137162511aa4999a39a
Laurent Grima <grima.laurent@gmail.com>
2017-11-28 20:20:12 +0100

    Strip unneeded content in App from boilerplate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
+// @flow
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+type Props = void;
+
+class App extends Component<Props> {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">Hello</div>;
   }
 }
 
