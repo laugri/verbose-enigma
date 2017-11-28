fc8ba2d7677348db7b2faf3d0bd07622af1c7571
Laurent Grima <grima.laurent@gmail.com>
2017-11-28 20:44:55 +0100

    Quickly hack display of list of books returned by the API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 // @flow
 
 import React, { Component } from 'react';
+import superagent from 'superagent';
+
 import './App.css';
 
-type Props = void;
+type Author = {
+  name: string,
+  slug: string,
+};
+type Book = {
+  authors: Array<Author>,
+  document: string,
+  id: string,
+  image: string,
+  largeImage: string,
+  mediumImage: string,
+  shortTitle: string,
+  slug: string,
+  title: string,
+};
+type Module = { type: string, books?: Array<Book> };
+type Response = {
+  id: string,
+  modules: Array<Module>,
+  slug: string,
+  title: string,
+};
 
-class App extends Component<Props> {
+type Props = {};
+type State = {
+  books: Array<Book>,
+};
+
+class App extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = { books: [] };
+  }
+
+  componentDidMount() {
+    superagent.get('https://api.glose.com/v1/booklists/free-books').then(
+      (response: { body: Response }) => {
+        const books = response.body.modules[1].books;
+        this.setState({ books: books || [] });
+      },
+      error => {
+        console.warn(error);
+      }
+    );
+  }
+
   render() {
-    return <div className="App">Hello</div>;
+    const { books } = this.state;
+    return (
+      <div className="App">
+        <ul>{books.map(book => <li key={book.slug}>{book.title}</li>)}</ul>
+      </div>
+    );
   }
 }
 
