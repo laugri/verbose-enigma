57f3210c3ebb807ee5970b61f26704aa716b378c
Laurent Grima <grima.laurent@gmail.com>
2017-11-29 15:33:49 +0100

    Add author line

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import type { Book as BookType } from 'types';
+import type { Book as BookType, Author } from 'types';
 import './book.css';
 
 type Props = {
@@ -9,13 +9,29 @@ type Props = {
 };
 
 class Book extends Component<Props> {
+  renderAuthorLine(authors: Array<Author>) {
+    // arbitrary display only first author.
+    if (authors.length) {
+      return (
+        <p className="Book__Author">
+          by <strong>{authors[0].name}</strong>
+        </p>
+      );
+    } else {
+      return <p className="Book__Author" />;
+    }
+  }
+
   render() {
     const { book } = this.props;
     return (
       <article className="Book">
         <img className="Book__Image" src={book.image} alt={book.title} />
         <header>
-          <h2 className="Book__Title">{book.title}</h2>
+          <h2 className="Book__Title" title={book.title}>
+            {book.title}
+          </h2>
+          {this.renderAuthorLine(book.authors)}
         </header>
       </article>
     );
